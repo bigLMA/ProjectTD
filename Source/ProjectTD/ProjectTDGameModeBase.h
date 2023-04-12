ddd275d36df415520e4f598f229afe2e61fafcae
sogasn <lashkomykhail@gmail.com>
2023-04-12 10:00:15 +0300

    Initial commit

---
+++ Source/ProjectTD/ProjectTDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectTDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTD_API AProjectTDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
