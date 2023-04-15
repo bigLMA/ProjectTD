bbc5327df4f4c6a5c3601eafb6df8c9ea8613a14
sogasn <lashkomykhail@gmail.com>
2023-04-15 15:12:10 +0300

    Added enemy base functionality
    - also added new module dependincies
    - created enemy base blueprint

--- Source/ProjectTD/Private/Enemy.cpp
+++ Source/ProjectTD/Private/Enemy.cpp
@@ -2,13 +2,30 @@
 
 
 #include "Enemy.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Kismet/GameplayStatics.h"
+#include "Path.h"
+#include "AIController.h"
 
 // Sets default values
 AEnemy::AEnemy()
+: Bounty(10), BountyBase(10), BountyDispersion(0), MaxHealth(50), DistanceThreshold(70)
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	// Intialize actor components
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(Root);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetupAttachment(Root);
+
+	Collision = CreateDefaultSubobject<USphereComponent>("Collision");
+	Collision->SetupAttachment(Mesh);
+
+	FloatingPawnComponent = CreateDefaultSubobject<UFloatingPawnMovement>("Pawn Movement");
 }
 
 // Called when the game starts or when spawned
@@ -16,13 +33,9 @@ void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
+	WalkToNextTarget();
 
-// Called every frame
-void AEnemy::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+	InitializeEnemy();
 }
 
 // Called to bind functionality to input
@@ -32,3 +45,164 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
+void AEnemy::RecieveDamage(int32 Damage)
+{
+	Health -= Damage;
+
+	if (Health <= 0)
+	{
+		Die();
+	}
+}
+
+void AEnemy::ApplyBurning(int32 BurningDamage, float BurningDuration, float BurningInterval)
+{
+	int32 Index = -1;
+
+	// Ñheck if enemy already has effect applied
+	if (CheckForEffect(EEffects::Burning, Index))
+	{
+		// Renew effect timer
+
+	}
+	else
+	{
+		// Apply effect
+	//	auto TakeBurningDamage = [&](int32 BurningDamage) {TakeDamageFromBurning(BurningDamage);
+
+		GetWorld()->GetTimerManager().SetTimer(BurningTimerHandle, 
+			this, &AEnemy::OnBurningDamageTimerFinished, BurningDuration, false);
+		GetWorld()->GetTimerManager().SetTimer(BurningDamageTimerHandle, [this, BurningDamage]()
+		{
+				TakeDamageFromBurning(BurningDamage);
+		}, BurningInterval, true);
+	}
+}
+
+// Called when bunribg efferct finishes
+void AEnemy::OnBurningDamageTimerFinished()
+{
+	GetWorld()->GetTimerManager().ClearTimer(BurningTimerHandle);
+	GetWorld()->GetTimerManager().ClearTimer(BurningDamageTimerHandle);
+}
+
+// Called when enemy takes damage over time from burning
+void AEnemy::TakeDamageFromBurning(int32 BurningDamage)
+{
+	RecieveDamage(BurningDamage);
+}
+
+// Apply slow on Enemy
+void AEnemy::ApplySlow(float SlowStrength, float SlowDuration)
+{
+	int Index = -1;
+
+	// Ñheck if enemy already has effect applied
+	if (CheckForEffect(EEffects::Slow, Index))
+	{
+		// Renew effect timer
+		GetWorld()->GetTimerManager().ClearTimer(SlowTimerHandle);
+		GetWorld()->GetTimerManager().SetTimer(SlowTimerHandle,
+			this, &AEnemy::RemoveSlow, SlowDuration, false);
+	}
+	else
+	{
+		Effects.Add(EEffects::Slow);
+
+		// Otherwise apply effect
+		FloatingPawnComponent->MaxSpeed *= (1 - SlowStrength);
+
+		// Set timer to remove slow
+		GetWorld()->GetTimerManager().SetTimer(SlowTimerHandle,
+			this, &AEnemy::RemoveSlow, SlowDuration, false);
+	}
+}
+
+// Removes slow from enemy
+void AEnemy::RemoveSlow()
+{
+	Effects.RemoveSingle(EEffects::Slow);
+	FloatingPawnComponent->MaxSpeed = InitialSpeed;
+}
+
+// Plays when enemy die
+void AEnemy::Die()
+{
+	RewardPlayer();
+	Destroy();
+}
+
+// Reward player with a bounty
+void AEnemy::RewardPlayer()
+{
+	//TODO
+}
+
+// Checks if enemy has that effect
+// Return -1 in index if false
+bool AEnemy::CheckForEffect(EEffects Effect, int32& Index)
+{
+	return false;
+}
+
+// Sets all enemy variables, that cannot be set on construction
+void AEnemy::InitializeEnemy()
+{
+	Health = MaxHealth;
+	InitialSpeed = FloatingPawnComponent->MaxSpeed;
+	RandomizeEnemyBounty();
+}
+
+// Called on BeginPlay to change bounty 
+void AEnemy::RandomizeEnemyBounty()
+{
+	Bounty = FMath::RandRange(BountyBase - BountyDispersion, BountyBase + BountyDispersion);
+}
+
+// Called on BeginPlay, and when enemy is closed to target
+void AEnemy::WalkToNextTarget()
+{
+	//if () { return; }
+
+	TArray<AActor*> Paths;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APath::StaticClass(), Paths);
+
+	if (auto Path = Cast<APath>(Paths[0]))
+	{
+		// Order AI to move to new location
+		++TargetIndex;
+		FVector TargetLocation;
+		Path->GetLocationFromIndex(TargetIndex, TargetLocation);
+
+		if (auto AIController = GetController<AAIController>())
+		{
+			AIController->MoveToLocation(TargetLocation);
+			GetWorld()->GetTimerManager().SetTimer(TimerProximityTimerHandle,
+				this, &AEnemy::CheckTargetProximity, 0.1f, true);
+		}
+	}
+}
+
+// Checks if enemy is closed to target
+void AEnemy::CheckTargetProximity()
+{
+	TArray<AActor*> Paths;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APath::StaticClass(), Paths);
+
+	if (auto Path = Cast<APath>(Paths[0]))
+	{
+		FVector TargetLocation;
+		Path->GetLocationFromIndex(TargetIndex, TargetLocation);
+
+		// Check if distance to target is less or equal then threshold
+		auto VDistance = GetActorLocation() - TargetLocation;
+		float Distance = VDistance.Size();
+		
+		if (Distance <= DistanceThreshold)
+		{
+			GetWorld()->GetTimerManager().ClearTimer(TimerProximityTimerHandle);
+			// Order enemy to walk to next target
+			WalkToNextTarget();
+		}
+	}
+}
--- Source/ProjectTD/Public/Enemy.h
+++ Source/ProjectTD/Public/Enemy.h
@@ -6,6 +6,12 @@
 #include "GameFramework/Pawn.h"
 #include "Enemy.generated.h"
 
+UENUM()
+enum class EEffects :uint8
+{
+	Slow, Burning
+};
+
 UCLASS()
 class PROJECTTD_API AEnemy : public APawn
 {
@@ -15,15 +21,115 @@ public:
 	// Sets default values for this pawn's properties
 	AEnemy();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// Called when enemy takes damage
+	UFUNCTION(BlueprintCallable, Category = "Damage")
+	void RecieveDamage(int32 Damage);
+
+	// Called when enemy is set on fire
+	UFUNCTION(BlueprintCallable, Category = "Burning")
+	void ApplyBurning(int32 BurningDamage, float BurningDuration, float BurningInterval);
+
+	// Called when burning damage timer has finished
+	UFUNCTION(BlueprintCallable, Category = "Burning")
+	void OnBurningDamageTimerFinished();
+
+	// Apply slow on Enemy
+	UFUNCTION(BlueprintCallable, Category = "Slow")
+	void ApplySlow(float SlowStrength, float SlowDuration);
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Maximum health of enemy
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health", meta = (ClampMin = 1, UIMin = 1))
+	int32 MaxHealth;
+
+	// Current health of enemy
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	int32 Health;
+
+	// Bounty value for killing enemy
+	UPROPERTY(BlueprintReadOnly, Category = "Rewards", meta = (ClampMin = 1, UIMin = 1))
+	int32 Bounty;
+
+	// Base value for calculating random bounty
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rewards", meta = (ClampMin = 1, UIMin = 1))
+	int32 BountyBase;
+
+	// Dispersion value for calculating random bounty
+	// Use 0 to fix bounty value
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rewards", meta = (ClampMin = 0, UIMin = 0))
+	int32 BountyDispersion;
+
+	// Initial speed of enemy
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float InitialSpeed;
+
+	// Enemy copmonents
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Root")
+	USceneComponent* Root;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mesh")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mesh")
+	class USphereComponent* Collision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	class UFloatingPawnMovement* FloatingPawnComponent;
+
+private:
+	// Called when enemy takes damage over time from burning
+	void TakeDamageFromBurning(int32 BurningDamage);
+
+	// Called on BeginPlay to change bounty 
+	void RandomizeEnemyBounty();
+
+	// Called on BeginPlay, and when enemy is closed to target
+	void WalkToNextTarget();
+
+	// Checks if enemy is closed to target
+	void CheckTargetProximity();
+
+	// Used to get location from path
+	int TargetIndex;
+
+	// Used to check enemy proximity to target
+	float DistanceThreshold;
+
+	// Removes slow from enemy
+	void RemoveSlow();
+
+	// Plays when enemy die
+	void Die();
+
+	// Reward player with a bounty
+	void RewardPlayer();
+
+	// Checks if enemy has that effect
+	// Return -1 in index if false
+	bool CheckForEffect(EEffects Effect, int32& Index);
+
+	// Sets all enemy variables, that cannot be set on construction
+	void InitializeEnemy();
+
+	// Timer handle for checking proximity to target
+	FTimerHandle TimerProximityTimerHandle;
+
+	// Timer handle for burning damage
+	FTimerHandle BurningDamageTimerHandle;
+
+	// Timer handle for burning effect
+	FTimerHandle BurningTimerHandle;
+
+	// Timer handle for slow effect
+	FTimerHandle SlowTimerHandle;
+
+	// Enemy effects
+	TArray<EEffects>Effects;
 };
